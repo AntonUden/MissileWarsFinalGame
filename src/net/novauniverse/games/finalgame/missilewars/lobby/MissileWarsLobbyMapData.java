136d16e099f0459b408c983873384f0466f4484b
Anton udén <udenanton@hotmail.com>
2023-04-03 14:33:17 +0200

    update to support latest version novacore

--- src/net/novauniverse/games/finalgame/missilewars/lobby/MissileWarsLobbyMapData.java
+++ src/net/novauniverse/games/finalgame/missilewars/lobby/MissileWarsLobbyMapData.java
@@ -2,11 +2,13 @@ package net.novauniverse.games.finalgame.missilewars.lobby;
 
 import java.util.ArrayList;
 
+import org.json.JSONObject;
+
 import net.zeeraa.novacore.spigot.gameengine.module.modules.gamelobby.map.GameLobbyMapData;
 import net.zeeraa.novacore.spigot.utils.LocationData;
 
 public class MissileWarsLobbyMapData extends GameLobbyMapData {
 	public MissileWarsLobbyMapData() {
-		super(new LocationData(0D, 0D, 0D), "MissileWars", "MissileWars", "Default waiting map for missilewars", null, new ArrayList<>());
+		super(new LocationData(0D, 0D, 0D), "MissileWars", "MissileWars", "Default waiting map for missilewars", null, new ArrayList<>(), new JSONObject());
 	}
 }
